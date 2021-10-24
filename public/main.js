a0865f38778f3f807abebe44a82a370c1932534a
Junior-pnascimento <junior.pnascimento@live.com>
2021-10-24 18:46:27 -0300

    first commit

---
+++ public/main.js
@@ -0,0 +1,34 @@
+const LinksSocialMedia = {
+    github: 'Junior-pnascimento',
+    youtube: 'channel/UCXgIJUNh-0E2M4QiJU48sIA',
+    facebook: 'junior.Sixx.7',
+    instagram: 'juniorsixx/',
+    twitter: 'JuniorSixXx',
+    linkedin: 'in/junior-nascimento-773982131/'
+}
+
+function changeSocialMediaLinks() {
+    for (let li of socialLinks.children) {
+        const social = li.getAttribute('class')
+
+        li.children[0].href = `https://${social}.com/${LinksSocialMedia[social]}`
+    }
+}
+
+changeSocialMediaLinks()
+
+function getGitHubProfileInfos() {
+    const url = `https://api.github.com/users/${LinksSocialMedia.github}`
+
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            userName.textContent = data.name
+            userBio.textContent = data.bio
+            userLink.href = data.html_url
+            UserImage.src = data.avatar_url
+            userLogin.textContent = data.login
+        })
+}
+
+getGitHubProfileInfos()
